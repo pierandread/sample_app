1a3d7db39d1e35d5e483700056ba8ef42020d97c
pierandreadelise <pier.delise@teladochealth.com>
2023-09-11 23:36:08 +0200

    8.1.2 done

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get "/about", to: 'static_pages#about'
   get "/contact", to: 'static_pages#contact'
   get "/signup", to: "users#new"
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#delete'
+
   resources :users
 end
