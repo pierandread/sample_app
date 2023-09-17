d9da15ca4193dbceb84875f8a507459a7a4c0257
Pier Andrea <pierandreadelise@gmail.com>
2023-09-18 00:29:30 +0200

    calling it a night

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,15 @@
 class SessionController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login
+    else
+      flash[:danger] = 'Invalid email/password combination' # no bueno
+      render 'new', status: :unprocessable_entity
+    end
   end
+
+  def destroy; end
 end
