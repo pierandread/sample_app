4c49c7d366d4f2db0ee18e3808a46f33db1f0548
Pier Andrea <pierandreadelise@gmail.com>
2023-09-24 01:23:32 +0200

    9.1.3 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   # or self.email = email.downcase
   validates :name, presence: true, length: { maximum: 50 }
@@ -14,5 +16,26 @@ class User < ApplicationRecord
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost:)
+  end
+
+  # random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # remember user in db for use in persistent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # true if given token matches the digest
+  # remember_digest same as self.remember_digest is created automatically by Active Record based on name of db column
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
