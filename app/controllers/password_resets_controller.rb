aa281e9ce439ff18421224c897e071a21d608a24
pierandreadelise <pier.delise@teladochealth.com>
2023-10-21 13:35:57 +0200

    12.3 missing last excercises

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,62 @@
+# frozen_string_literal: true
+
 class PasswordResetsController < ApplicationController
-  def new
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: [:edit, :update]
+
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = 'Email sent with password reset instruction'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Email address not found'
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      @user.forget
+      log_in @user
+      flash[:success] = "Password had been reset."
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    return if @user&.activated? && @user&.authenticated?(:reset, params[:id])
+
+    redirect_to root_url
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = 'Password reset has expired'
+      redirect_to new_password_reset_url
+    end
   end
 end
