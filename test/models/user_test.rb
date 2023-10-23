e904337c4a61ace04e33f41b484a9a0a01687d6c
pierandreadelise <pier.delise@teladochealth.com>
2023-10-23 13:39:06 +0200

    13.2.2 done

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,4 +75,12 @@ class UserTest < ActiveSupport::TestCase
   test 'authenticated? should return false for user with nil digest' do
     assert_not @user.authenticated?(:remember,'')
   end
+
+  test 'associated microposts should be destroyed' do
+    @user.save
+    @user.microposts.create!(content: 'To be deleted')
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
