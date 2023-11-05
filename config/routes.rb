67f88871e333db8f5a5dde0567b52ba85c1f1d47
pierandreadelise <pier.delise@teladochealth.com>
2023-11-06 00:07:47 +0100

    page 724 todo

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/microposts', to: 'static_pages#home'
 
   resources :users
   resources :account_activations, only: [:edit]
