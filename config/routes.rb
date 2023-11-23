93ce671cfe269d0bc9f0eaeacf19cf8169374030
pierandreadelise <pier.delise@teladochealth.com>
2023-11-23 18:11:58 +0100

    pag 790

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'password_resets/new'
   get 'password_resets/edit'
-  root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
@@ -16,9 +17,9 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resource :relationships, only: [:create, :destroy]
 end
